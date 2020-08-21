d20ec8aa45ef8eb203ea47337849ef14fd6c7ecd
edsky <dead.ash@hotmail.com>
2020-08-21 13:28:33 +0000

    修复32位linux问题

--- libunicorn-sys/build.rs
+++ libunicorn-sys/build.rs
@@ -7,24 +7,18 @@ fn main() {
 
 #[cfg(macos)]
 fn get_libc_path() -> &'static str {
-    "/usr/local/lib"
+    "/usr/local/lib64"
 }
 
 #[cfg(not(macos))]
 #[allow(dead_code)]
 fn get_libc_path() -> &'static str {
-    "/usr/lib"
+    "/usr/lib64"
 }
 
 #[cfg(not(windows))]
 fn main() {
-    let path:String;
-    if Some(64) == build_helper::target::pointer_width() {
-        path = get_libc_path().to_owned() + "64";
-    } else {
-        path = get_libc_path().to_owned();
-    }
     build_helper::rustc::link_search(
-        Some(build_helper::SearchKind::Native), path);
+        Some(build_helper::SearchKind::Native), get_libc_path());
     build_helper::rustc::link_lib(Some(build_helper::LibKind::Static), "unicorn");
 }
